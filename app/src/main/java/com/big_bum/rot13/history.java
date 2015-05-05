b1e039db671d3c4f71f1c35cf72b39724213217b
Stefanescu Cristian <howerkraft@gmail.com>
2015-05-05 23:29:40 +0300

    test android studio
    Signed-off-by: Stefanescu Cristian <howerkraft@gmail.com>

---
+++ app/src/main/java/com/big_bum/rot13/history.java
@@ -0,0 +1,15 @@
+package com.big_bum.rot13;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class history extends Activity {
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history);
+    }
+    
+}
